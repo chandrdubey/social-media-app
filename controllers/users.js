747b351d659a802d08f7cff5b0152f3ae0a1265f
Chandra Dubey <chandraprak33@gmail.com>
2021-06-25 03:15:43 +0530

    user delete api careted

--- controllers/users.js
+++ controllers/users.js
@@ -3,27 +3,32 @@ const pool = require('../config/postgres');
 //     "SELECT * FROM users;")
 //     .then(result => console.log(result.rows))
 //     .catch(err => console.log(err))
+const admins_id = 1;
 module.exports = {
     signup : ( req, res)=>{
         const {name , email, active, mobile_number } = req.body;
         pool.query(
             "INSERT INTO users (active,name,email,mobile_number) VALUES ($1, $2 ,$3 ,$4)",
             [active, name, email, mobile_number],)
-            .then(result => res.status(200).json({result:result.rows,message: "user signup successful"}))
+            .then(result => res.status(200).json({message: "user signup successful"}))
             .catch(err => console.log(`there is an error ${err}`))
     },
+    
+    // GET request for all the users 
     getAllUsers: (req, res) =>{
         pool.query(
-            "SELECT * FROM users ORDER BY id;")
+            "SELECT * FROM users ORDER BY id")
             .then(result => {
                res.status(200).json({ result :result.rows});
             })
             .catch(err => console.log(`there is an error ${err}`));
     },
+
+    // GET request for specific user
     getUserById: (req, res)=>{
         console.log(req.params);
         pool.query(
-            "SELECT * FROM users WHERE id=$1 ORDER BY id;",[])
+            "SELECT * FROM users WHERE id=$1 ORDER BY id",[req.params.user_id])
             .then(result => {
                if(result.rows.length > 0){
                 res.status(200).json({ result :result.rows});
@@ -33,9 +38,11 @@ module.exports = {
             })
             .catch(err => console.log(`there is an error ${err}`));
     },
+
+    // GET request for all the active users
     activeUsers: (req, res)=>{
         pool.query(
-            "SELECT * FROM users WHERE active=$1 ORDER BY id;", [1])
+            "SELECT * FROM users WHERE active=$1 ORDER BY id", [1])
             .then(result => {
                 console.log(result);
                if(result.rows.length > 0){
@@ -46,6 +53,34 @@ module.exports = {
             })
             .catch(err => console.log(`there is an error ${err}`));
     },
-    
-    
+    // Delete data only for user
+    userDelete: (req, res) =>{
+          const {user_id} = req.params;
+          pool.query("SELECT * FROM users WHERE id=$1",[user_id])
+          .then(result =>{
+              if(result.rows.length>0){
+                  if(result.rows[0].active){
+                      pool.query("UPDATE users SET active=$1 WHERE id=$2", [0, user_id])
+                      .then(() =>res.status(200).json({message:"ok"}))
+                      .catch(err =>console.error(err))
+                  }else{
+                      return res.status(400).json({message:"Invalid request"});
+                  }
+              }else{
+                  return res.status(404).json({message :"User not found"});
+              }
+          })
+    },
+    // DELETE user data permanently
+    userDeletePermanent : (req, res)=>{
+        const {id} = req.body;
+        console.log(id);
+        if(parseInt(id) === admins_id){
+            pool.query("DELETE FROM users WHERE id=$1 RETURNING*", [req.params.user_id])
+            .then(result => res.status(200).json({result: result.rows[0], message:"User Deleted Succesfulley"}))
+            .catch(err => console.error(err))
+        }else{
+            res.status(401).json({message:"Unautherized request"});
+        } 
+    }    
 }
--- routes/users.js
+++ routes/users.js
@@ -6,5 +6,7 @@ router.post('/new', usersController.signup);
 router.get('/', usersController.getAllUsers);
 router.get('/active', usersController.activeUsers);
 router.get('/:user_id', usersController.getUserById);
+router.put('/:user_id/delete', usersController.userDelete);
+router.delete('/:user_id/delete', usersController.userDeletePermanent);
 
 module.exports = router;
